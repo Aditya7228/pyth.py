da76a2fc5754d3d8ca1482de3260cf3a82a63aa0
Aditya7228 <adityajagtap966@gmail.com>
2026-01-27 13:49:23 +0530

    updated project files

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def home():
 import os
 if __name__=="__main__":
     app.run(host="0.0.0.0",
-            port=int(os.environ.get("PORT,5000")))
+            port=int(os.environ.get("PORT",5000)))
